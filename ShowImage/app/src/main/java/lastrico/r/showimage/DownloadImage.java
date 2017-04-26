d7e409e511e4aa53f3d54528003c74e40624843d
rick <riky.lastrico@gmail.com>
2017-04-26 17:11:37 +0200

    aggiunta la interface

--- ShowImage/app/src/main/java/lastrico/r/showimage/DownloadImage.java
+++ ShowImage/app/src/main/java/lastrico/r/showimage/DownloadImage.java
@@ -1,27 +1,95 @@
 package lastrico.r.showimage;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
-        import android.os.AsyncTask;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.ImageView;
 
-/**
- * Created by feder on 05/04/2017.
- */
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class DownloadImage extends AsyncTask<String, Void, Bitmap> { /* l'AsyncTask richiede tre tipi
-uno è il tipo che si aspeta in input Backgroung (URL dell'immagine), l'ultimo è quello che restituisce*/
+uno è il tipo che si aspetta in input background (URL dell'immagine), l'ultimo è quello che restituisce*/
+
+    private final String tag="DownloadImage";
+
+    private ProgressDialog myPd=null;
+    private String myUrl=null;
+    private ImageView myIw=null;
+
+    private DownloadEnded myDe;
+
+    DownloadImage(ProgressDialog _pd, String _url, ImageView _iw, DownloadEnded _de)
+    {
+        myPd=_pd;
+        myUrl=_url;
+        myIw=_iw;
+        myDe=_de;
+    }
+
+
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+
+        myPd.setIndeterminate(true); //set del progressbar
+        myPd.setTitle("Please Wait");
+        myPd.setMessage("Download in progress");
+        myPd.show();
     }
 
     @Override
-    protected Bitmap doInBackground(String... params) {
-        return null;
+    protected Bitmap doInBackground(String... params) {   //non può avere a che fare con l'interfaccia grafica (doinbackgroung)
+        Bitmap bm=null;
+        try {
+            bm = DownloadImage(myUrl);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+        return bm;
     }
 
     @Override
-    protected void onPostExecute(Bitmap bitmap) {   //prende in ingresso il III oggetto delle parentesi angolari
+    protected void onPostExecute(Bitmap bitmap) {   //prende in ingresso il terzo oggetto delle parentesi angolari
         super.onPostExecute(bitmap);
+
+        myPd.dismiss();
+        //myIw.setImageBitmap(bitmap);
+
+        myDe.onDownloadFinished(bitmap);
+    }
+
+    private Bitmap DownloadImage(String _url) throws IOException
+    {
+        Bitmap bm = null;
+        URL url=new URL(_url);
+
+        //open url connection
+        HttpURLConnection urlConnection=(HttpURLConnection) url.openConnection();
+        urlConnection.connect();
+
+        //open input stream channel
+        InputStream is= urlConnection.getInputStream();
+        BufferedInputStream bis= new BufferedInputStream(is);
+
+        bm= BitmapFactory.decodeStream(is);
+
+
+        bis.close();
+        is.close();
+
+        return bm;
     }
 }
--- ShowImage/app/src/main/java/lastrico/r/showimage/MainActivity.java
+++ ShowImage/app/src/main/java/lastrico/r/showimage/MainActivity.java
@@ -1,5 +1,7 @@
 package lastrico.r.showimage;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,13 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DownloadEnded{
 
     Button button=null;
     EditText text=null;
-    ImageView image=null;
+    ImageView iw =null;
+
+    DownloadImage myImage=null;
+
+    private ProgressDialog progressdialog=null;
+    private DownloadEnded de=null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +29,45 @@ public class MainActivity extends AppCompatActivity {
 
         button=(Button) findViewById(R.id.bttButton);
         text=(EditText)findViewById(R.id.etText);
-        image=(ImageView)findViewById(R.id.ivImage);
+        iw =(ImageView)findViewById(R.id.ivImage);
+
+        new Thread(new Runnable() { //lanciare un nuovo thread con una funzione pesante
+            @Override               // non funziona bene come l'asinc task, è più semplice
+            public void run() {     //come sintassi
+                VeryHighLoad();
+            }
+        });
+
+        de=this; //l'oggetto si inizializza con la classe stessa
+
+
+        progressdialog=new ProgressDialog(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String _s = text.getText().toString();
 
+                myImage = new DownloadImage(progressdialog, _s, iw, de);
+
+                myImage.execute(new String[] {""});
 
             }
         });
 
+
+    }
+
+    @Override
+    public void onDownloadFinished (Bitmap bm)
+    {
+        iw.setImageBitmap(bm);
+
+
+    }
+
+    private void VeryHighLoad()
+    {
 
     }
 }
