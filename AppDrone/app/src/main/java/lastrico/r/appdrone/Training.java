5a61c729d06fb782b6913fbf41c5f32c945edef0
Riccardo Lastrico <riky.lastrico@gmail.com>
2017-05-07 21:54:48 +0200

    appdrone implementazione ale

--- AppDrone/app/src/main/java/lastrico/r/appdrone/Training.java
+++ AppDrone/app/src/main/java/lastrico/r/appdrone/Training.java
@@ -1,8 +1,12 @@
 package lastrico.r.appdrone;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,12 +18,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Training  extends Activity {
+public class Training  extends Activity  implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    TextView tvdisplay;
+
+    ImageView myImageView = null;
+    //TextView myTextView = null;
+    Button bttClickMe = null;
+    Button bttClickMe2 = null;
+    Button bttClickMe4 = null;
+    Bitmap myBitmap = null;
+    int REQUEST_IMAGE_CAPTURE = 1;
+    ProgressDialog progress;
+    SeekBar mySeekBar;
+    ManipulateImage _myCV;
+    //int manipulationSelected = 1;
+
+    //Function to launch camera application
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +56,134 @@ public class Training  extends Activity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                dispatchTakePictureIntent();
             }
         });
-        TextView tvdisplay;
-        String cibosel="sas";
-        tvdisplay=(TextView)findViewById(R.id.tvdisplay);
 
-        tvdisplay.setText(cibosel);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_training);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
+        myImageView = (ImageView) findViewById(R.id.etImage);
+        bttClickMe2 = (Button) findViewById(R.id.bttClickMe2);
+        bttClickMe4 = (Button) findViewById(R.id.bttClickMe4);
+        progress = new ProgressDialog(this);
+        mySeekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        //initialize SeekBar correctly
+        mySeekBar.setProgress(128); //128 ad default value
+        mySeekBar.setMax(255);
+
+        //initialize ImageView content
+        myBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        myImageView.setImageBitmap(myBitmap); //set ImageView content
+
+        //------------ BUTTONS & SEEKBAR LISTENERS ------------
+
+        //gray scale button
+        bttClickMe2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _myCV = new ManipulateImage(progress, myImageView, myBitmap, 1);
+                _myCV.execute("");
+            }
+        });
+        //reset button
+        bttClickMe4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myImageView.setImageBitmap(myBitmap);
+            }
+        });
+
+        //Binary with Seek bar threshold manipulation
+        mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //visualize number selected with toast
+                Toast.makeText(Training.this,"Soglia: "+ progressChanged, Toast.LENGTH_SHORT).show();
+
+                //execute manipulation
+                _myCV = new ManipulateImage(progress, myImageView, myBitmap, 2, progressChanged);
+                _myCV.execute("");
+            }
+        });
 
 
 
 
 
 
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            myBitmap = imageBitmap;
+            myImageView.setImageBitmap(imageBitmap);
+        }
+    }
+    //Function for saving state of activity (for example when going landscape)
+    @Override
+    public void onSaveInstanceState(Bundle toSave) {
+        super.onSaveInstanceState(toSave);
+        toSave.putParcelable("bitmap", myBitmap);
+    }
+
+    //Function for reloading state of activity
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        myBitmap = savedInstanceState.getParcelable("bitmap");
+        myImageView.setImageBitmap(myBitmap);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.training) {
+
+
+
+        } else if (id == R.id.recognition) {
+            Intent manda = new Intent( getApplicationContext(),Recognition.class);
+
+            startActivity(manda);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
